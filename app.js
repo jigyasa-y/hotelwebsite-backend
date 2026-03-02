be458d7532a26b979f086f56ba403abed8551cb9
jigyasa-y <jigyasayadav361@gmail.com>
2026-03-03 01:24:46 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ import reservationRoute from "./routes/reservationRoute.js"
 const app=express();
 dotenv.config({path: "./config/config.env"});
 app.use(cors({
-  origin: [process.env.FRONTEND_URL],
+  origin: [https://hotelwebsite-5z8y7g6yg-jigyasas-projects-b6074222.vercel.app],
   methods: ["POST"],
   credentials: true
 }));
